94e6d17cabc781891c5933fe08eba9c837de1228
Alejandro Martinez <alewayna@gmail.com>
2022-06-14 00:22:27 -0300

    Step two completed

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ def pedir_letra():
     letra = input("Adivina la letra: ")
     return letra
 
-print(pedir_letra())
+
 
 
 #PASO 2: Crear un loop para pedir letras al usuario
+
+intentos = 0
+while intentos < 5:
+    print(pedir_letra())
+    intentos += 1
