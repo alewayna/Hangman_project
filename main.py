f3573a32b65f1ea560a02b7b06edd9e6be8e4788
Daniel Pedroza <danie.p.usb@gmail.com>
2022-06-14 01:50:39 -0300

    Step six, seven and eigth

--- main.py
+++ main.py
@@ -5,8 +5,18 @@
 ##          cuando ya adivinó la palabra
 ##          cuando pasaron 5 intentos 
 #PASO 4: Buscar la letra en una palabra
-#PASO 5:    si la encuentro, entonces...
-##          si NO la encuentro, entonces...
+#PASO 5:    si la encuentro, agregar la letra en la lista de letras correctas
+##          si NO la encuentro, sumar uno a la cantidad de intentos actuales 
+###                             agregar la letra a la lista de letras incorrectas
+#PASO 6: Crear una funcion que escriba en pantalla los espacios de la palabra que se pase por parametros.
+##          ejemplo: mostrar_espacios("daniel") entonces arrojará  _ _ _ _ _ _
+#PASO 7: Agregar a la funcion mostrar_espacios, un segundo parametro que será una lista de letras correctas
+##          deberás imprimir los espacios aun no descubiertos y ademas los espacios de las letras encontradas
+##           HINT(usa la funcion find() )            
+#PASO 8: En cada iteracion, imprimir en pantalla las letras incorrectas.
+##          es decir, recorrer la lista de letras incorrectas e imprimirlas
+
+
 
 ##Funciones
 def pedir_letra():
