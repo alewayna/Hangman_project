be0d051cd221c38fdfa2c6108ae74cc89b089038
Daniel Pedroza <danie.p.usb@gmail.com>
2022-06-14 00:43:54 -0300

    step three fixed

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #HANGMAN
-#PASO 1: lograr pedir una letra al usuario y guardar letra
+#PASO 1: Lograr pedir una letra al usuario y guardar letra
 #PASO 2: Crear un loop para pedir letras al usuario
-#PASO 3:    condicionar el loop para que se detenga:
+#PASO 3: Condicionar el loop para que se detenga:
 ##          cuando ya adivinó la palabra
 ##          cuando pasaron 5 intentos 
 
@@ -10,9 +10,11 @@ def pedir_letra():
     letra = input("Adivina la letra: ")
     return letra
 
+maximo_cantidad_de_intentos     = 5
+cantidad_de_intentos_actuales   = 0
+esta_el_juego_terminado         = False
 
-cantidad_de_intentos = 0
-juego_terminado = False
-while cantidad_de_intentos < 5 and not juego_terminado:
+
+while cantidad_de_intentos_actuales < maximo_cantidad_de_intentos and not esta_el_juego_terminado:
     print(pedir_letra())
-    cantidad_de_intentos += 1
+    cantidad_de_intentos_actuales += 1
