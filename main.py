47c2f967f1beb93cbf28c4b863624685929db7d7
Daniel Pedroza <danie.p.usb@gmail.com>
2022-06-14 01:10:54 -0300

    step five

--- main.py
+++ main.py
@@ -5,23 +5,30 @@
 ##          cuando ya adivinó la palabra
 ##          cuando pasaron 5 intentos 
 #PASO 4: Buscar la letra en una palabra
+#PASO 5:    si la encuentro, entonces...
+##          si NO la encuentro, entonces...
 
+##Funciones
 def pedir_letra():
     letra = input("Adivina la letra: ")
     return letra
 
+
+#Variables
 maximo_cantidad_de_intentos     = 5
 cantidad_de_intentos_actuales   = 0
 seguir_el_juego                 = True
 palabra                         = "dinosaurio"
 letra_a_buscar                  = ''
 
+
+
+#Proceso principal
 while cantidad_de_intentos_actuales < maximo_cantidad_de_intentos and seguir_el_juego:
     letra_a_buscar = pedir_letra()
     print(letra_a_buscar in palabra)
 
-        
 
-
+    
     cantidad_de_intentos_actuales += 1
     
