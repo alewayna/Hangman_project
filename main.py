9e21df965fc48491714a78e8aa80fc58079f7264
Alejandro Martinez <alewayna@gmail.com>
2022-06-26 22:41:14 -0300

    Step 7 completed

--- main.py
+++ main.py
@@ -26,31 +26,52 @@ def pedir_letra():
     letra = input("Adivina la letra: ")
     return letra
 
+
 def mostrar_espacios(palabra):
+    """Esta funcion recibe una palabra e imprime en pantalla el caracter "_" 
+    repetido la cantidad de letras de la palabra pasada por parámetro"""
     incognito = ""
     for l in palabra:
         incognito += "_ "
     print(incognito)
 
+# Paso 7?
+def guardar_letra(letra_a_guardar):
+    lista_de_letras_introducidas_por_el_usuario.append(letra_a_guardar)
+    
 
 #Variables
 maximo_cantidad_de_intentos     = 5
 cantidad_de_intentos_actuales   = 0
 seguir_el_juego                 = True
-palabra                         = "dinosaurio"
+lista_de_palabras               = ['queso', 'elefante', 'dinosaurio', 'juguete', 'tomate', 'huevo']
 letra_a_buscar                  = ''
 lista_de_letras_correctas       = []
 lista_de_letras_incorrectas     = []
+lista_de_letras_introducidas_por_el_usuario = []
+palabra = 'dinosaurio'
 
-
+mostrar_espacios(palabra)
 
 #Proceso principal
 while cantidad_de_intentos_actuales < maximo_cantidad_de_intentos and seguir_el_juego:
-    letra_a_buscar = pedir_letra()
-    print(letra_a_buscar in palabra)
+    
+    letra = pedir_letra()
+    print(f"La letra que colocaste es: {letra}")
+    
+
+    esta_en_palabra = False
+    # se usa el operador de pertenencia IN para devolver un booleano
+    esta_en_palabra = letra in palabra
+    if esta_en_palabra == True:
+        print("La letra esta en la palabra!")
+        lista_de_letras_correctas.append(letra)
+    else:
+        print("La letra no esta en la palabra ")
+        lista_de_letras_incorrectas.append(letra)
+    
     mostrar_espacios(palabra)
-
-
+    # Si esta en la palabra, imprimir mensaje está
+    # Si no está en la palabra, imprimir no está
     
-    cantidad_de_intentos_actuales += 1
-    
+    cantidad_de_intentos_actuales += 1    
